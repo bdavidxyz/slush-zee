da8c92d75827c7b15b4baa45bea9df11aafabc07
David <davbohr@gmail.com>
2016-08-09 08:22:43 +0200

    wip

--- slushfile.js
+++ slushfile.js
@@ -8,6 +8,11 @@
 
 'use strict';
 
+var sections = {
+    hero1: ['html', 'css'],
+    navbar1: ['html', 'css', 'js']
+};
+
 var gulp = require('gulp'),
     install = require('gulp-install'),
     conflict = require('gulp-conflict'),
@@ -15,22 +20,23 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     _ = require('underscore.string'),
     inquirer = require('inquirer'),
-    path = require('path');
+    path = require('path'),
+    replace = require('gulp-replace'),
+    insert = require('gulp-insert');
 
 function format(string) {
     var username = string.toLowerCase();
     return username.replace(/\s/g, '');
 }
 
-var defaults = (function () {
+var defaults = (function() {
     var workingDirName = path.basename(process.cwd()),
-      homeDir, osUserName, configFile, user;
+        homeDir, osUserName, configFile, user;
 
     if (process.platform === 'win32') {
         homeDir = process.env.USERPROFILE;
         osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
-    }
-    else {
+    } else {
         homeDir = process.env.HOME || process.env.HOMEPATH;
         osUserName = homeDir && homeDir.split('/').pop() || 'root';
     }
@@ -50,7 +56,17 @@ var defaults = (function () {
     };
 })();
 
-gulp.task('default', function (done) {
+
+
+
+/*______________________________________________________________________________________
+ *
+ *
+ * Default 
+ *
+ *______________________________________________________________________________________
+ */
+gulp.task('default', function(done) {
     var prompts = [{
         name: 'appName',
         message: 'What is the name of your project?',
@@ -81,14 +97,14 @@ gulp.task('default', function (done) {
     }];
     //Ask
     inquirer.prompt(prompts,
-        function (answers) {
+        function(answers) {
             if (!answers.moveon) {
                 return done();
             }
             answers.appNameSlug = _.slugify(answers.appName);
             gulp.src(__dirname + '/templates/**')
                 .pipe(template(answers))
-                .pipe(rename(function (file) {
+                .pipe(rename(function(file) {
                     if (file.basename[0] === '_' && file.basename[1] === '_') {
                         file.basename = '.' + file.basename.slice(2);
                     }
@@ -96,8 +112,78 @@ gulp.task('default', function (done) {
                 .pipe(conflict('./'))
                 .pipe(gulp.dest('./'))
                 .pipe(install())
-                .on('end', function () {
+                .on('end', function() {
                     done();
                 });
         });
+});
+
+
+
+
+
+
+
+/*______________________________________________________________________________________
+ *
+ *
+ * Add section
+ *
+ *______________________________________________________________________________________
+ */
+gulp.task('addsection', function(done) {
+
+    var prompts = [{
+        name: 'sectionToAdd',
+        message: 'Which section you want to add ?',
+        type: 'list',
+        choices: Object.keys(sections)
+    }
+    // ,
+    // {
+    //     name: 'sectionRenamed',
+    //     message: '(optional) Do you want a more specific name for this section ?'
+    // }
+    ];
+
+    //Ask
+    inquirer.prompt(prompts,
+        function(answers) {
+
+            var sectionToAdd = answers.sectionToAdd;
+            console.log(sectionToAdd);
+            if (sections[sectionToAdd] == null) { //null or undefined
+                console.log('Sorry, this section doesnt exist. Choose amongst ' + Object.keys(sections));
+            } else {
+                var webLangages = sections[sectionToAdd]
+                if (webLangages.indexOf('html') > -1) {
+                    addHTML(sectionToAdd);
+                }
+                if (webLangages.indexOf('css') > -1) {
+                    addCSS(sectionToAdd);
+                }
+                if (webLangages.indexOf('js') > -1) {
+                    addJS(sectionToAdd);
+                }
+            }
+
+            function addHTML(sectionName) {
+                gulp.src('./index.html')
+                  .pipe(insert.append('{% include html/zee/'+ sectionName +'.html %}\r'))
+                  .pipe(gulp.dest('./'));
+            }
+            function addCSS(sectionName) {
+                gulp.src('./css/main.scss')
+                  .pipe(insert.append('@import "zee/'+ sectionName +'";\r'))
+                  .pipe(gulp.dest('./css'));
+            }
+            function addJS(sectionName) {
+                gulp.src('./_layouts/default.html')
+                  .pipe(replace(/<!--endjs-->/, '  {% include javascript/zee/'+ sectionName + '.js.html %}\r\t\t<!--endjs-->'))
+                  .pipe(gulp.dest('./_layouts'));
+            }
+
+
+        });
+
 });
